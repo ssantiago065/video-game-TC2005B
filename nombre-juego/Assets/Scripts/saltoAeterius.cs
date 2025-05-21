475e80361cda710def06e3421b35a0fa53db6168
Luis Fernando Rojo Valdés <A01640584@tec.mx>
2025-05-20 19:26:58 -0600

    wall slide y wall jump

--- nombre-juego/Assets/Scripts/saltoAeterius.cs
+++ nombre-juego/Assets/Scripts/saltoAeterius.cs
@@ -3,7 +3,7 @@ using UnityEngine; //libreria necesaria para usar scripts en unity
 public class saltoAeterius : MonoBehaviour
 {
     public float speed = 5f; //velocidad de movimiento del personaje punto flotante
-    public float jumpForce = 10f; // cantidad de fuerza del salto
+    public float jumpForce = 6f; // cantidad de fuerza del salto
     private Rigidbody2D rb;
     private Vector2 moveInput; 
     public bool isGrounded; //bandera para saber si está en el piso.
@@ -11,6 +11,19 @@ public class saltoAeterius : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private bool facingRight = true;
     public saltoSolaris solaris;
+    private bool isWallSliding;
+    private float velocidadWallSlide = 2f;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Transform wallCheck;
+    private float horizontal;
+    private bool isWallJumping;
+    private float direccionWallJump;
+    private float tiempoWallJump = 1f;
+    private float contadorWallJump;
+    private Vector2 fuerzaWallJump = new Vector2(8f, 10f);
+
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); //variable que almacena un rigibody
@@ -21,26 +34,34 @@ public class saltoAeterius : MonoBehaviour
     {
         moveInput.x = Input.GetAxis("Horizontal");
         //moveInput.y = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
 
         // Detectar salto con barra espaciadora o clic del mouse
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
+
+        wallSlide();
+        wallJump();
+
     }
 
     void FixedUpdate()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+        if (!isWallJumping)
+        {
+            float moveX = Input.GetAxisRaw("Horizontal");
+            rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
 
-        if (moveX > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (moveX < 0 && facingRight)
-        {
-            Flip();
+            if (moveX > 0 && !facingRight && !isWallJumping)
+            {
+                Flip();
+            }
+            else if (moveX < 0 && facingRight && !isWallJumping)
+            {
+                Flip();
+            }
         }
     }
 
@@ -71,6 +92,59 @@ public class saltoAeterius : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             cooldownSalto = false;
         }
+    }
+
+    private bool isWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void wallSlide()
+    {
+        if (isWalled() && !isGrounded && horizontal != 0f)
+        {
+            isWallSliding = true;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Clamp(rb.linearVelocity.y, -velocidadWallSlide, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private void wallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            direccionWallJump = -transform.localScale.x;
+            contadorWallJump = tiempoWallJump;
+
+            CancelInvoke(nameof(stopWallJump));
+        }
+        else
+        {
+            contadorWallJump -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && contadorWallJump > 0f)
+        {
+            rb.linearVelocity = new Vector2(fuerzaWallJump.x * direccionWallJump, fuerzaWallJump.y);
+            isWallJumping = true;
+            contadorWallJump = 0f;
+
+            if (transform.localScale.x != direccionWallJump)
+            {
+                Flip();
+            }
+            
+            Invoke(nameof(stopWallJump), tiempoWallJump);
+        }
+    }
+
+    private void stopWallJump()
+    {
+        isWallJumping = false;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
